8edd388cc9b4c8b9778a0398a30f02203e4a4adc
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-09-19 11:43:50 -0300

    feat: add redux library and configure it

---
+++ src/app/redux/store.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { combineReducers, configureStore  } from "@reduxjs/toolkit";
+import counterReducer from "./features/counterSlice";
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+},);
+
+export const store = configureStore({
+    reducer: rootReducer
+});
